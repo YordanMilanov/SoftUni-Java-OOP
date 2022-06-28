267dfea4cada921039aff704a36cfa757fdd4e7e
YordanMilanov <dancho.galinov@gmail.com>
2022-06-28 11:05:24 +0300

    all tasks 100/100

---
+++ 02.WorkingWithAbstractions-Exercise/src/GreedyTimes_06/Main.java
@@ -0,0 +1,97 @@
+package GreedyTimes_06;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        long input = Long.parseLong(scanner.nextLine());
+        String[] items = scanner.nextLine().split("\\s+");
+
+        LinkedHashMap<String, LinkedHashMap<String, Long>> bag = new LinkedHashMap<>();
+        long gold = 0;
+        long stone = 0;
+        long money = 0;
+
+        for (int i = 0; i < items.length; i += 2) {
+            String name = items[i];
+            long quantity = Long.parseLong(items[i + 1]);
+
+            String type = "";
+
+            if (name.length() == 3) {
+                type = "Cash";
+            } else if (name.toLowerCase().endsWith("gem")) {
+                type = "Gem";
+            } else if (name.toLowerCase().equals("gold")) {
+                type = "Gold";
+            }
+
+            if (type.equals("")) {
+                continue;
+            } else if (input < bag.values().stream().map(Map::values).flatMap(Collection::stream).mapToLong(e -> e).sum() + quantity) {
+                continue;
+            }
+
+            switch (type) {
+                case "Gem":
+                    if (!bag.containsKey(type)) {
+                        if (bag.containsKey("Gold")) {
+                            if (quantity > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
+                                continue;
+                            }
+                        } else {
+                            continue;
+                        }
+                    } else if (bag.get(type).values().stream().mapToLong(e -> e).sum() + quantity > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
+                        continue;
+                    }
+                    break;
+                case "Cash":
+                    if (!bag.containsKey(type)) {
+                        if (bag.containsKey("Gem")) {
+                            if (quantity > bag.get("Gold").values().stream().mapToLong(e -> e).sum()) {
+                                continue;
+                            }
+                        } else {
+                            continue;
+                        }
+                    } else if (bag.get(type).values().stream().mapToLong(e -> e).sum() + quantity > bag.get("Gem").values().stream().mapToLong(e -> e).sum()) {
+                        continue;
+                    }
+                    break;
+            }
+
+            if (!bag.containsKey(type)) {
+                bag.put((type), new LinkedHashMap<String, Long>());
+            }
+
+            if (!bag.get(type).containsKey(name)) {
+                bag.get(type).put(name, 0L);
+            }
+
+
+            bag.get(type).put(name, bag.get(type).get(name) + quantity);
+            if (type.equals("Gold")) {
+                gold += quantity;
+            } else if (type.equals("Gem")) {
+                stone += quantity;
+            } else if (type.equals("Cash")) {
+                money += quantity;
+            }
+        }
+
+        for (Map.Entry<String, LinkedHashMap<String, Long>> x : bag.entrySet()) {
+            Long sumValues = x.getValue().values().stream().mapToLong(l -> l).sum();
+
+            System.out.println(String.format("<%s> $%s", x.getKey(), sumValues));
+
+            x.getValue().entrySet().stream().sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey())).forEach(i -> System.out.println("##" + i.getKey() + " - " + i.getValue()));
+
+        }
+    }
+}
