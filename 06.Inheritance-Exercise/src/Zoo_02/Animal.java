b49bccfc0617f3622f5d4cdc2aa8f9222b7201b7
YordanMilanov <dancho.galinov@gmail.com>
2022-07-11 11:48:36 +0300

    Zoo_02 100/100

---
+++ 06.Inheritance-Exercise/src/Zoo_02/Animal.java
@@ -0,0 +1,15 @@
+package Zoo_02;
+
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+
+}
