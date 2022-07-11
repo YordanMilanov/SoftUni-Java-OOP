967caa79c5660881968ec2b2f80652c90577a412
YordanMilanov <dancho.galinov@gmail.com>
2022-07-11 12:40:49 +0300

    PlayersAndMonsters_03 100/100

---
+++ 06.Inheritance-Exercise/src/PlayersAndMonsters_03/Main.java
@@ -0,0 +1,8 @@
+package PlayersAndMonsters_03;
+
+public class Main {
+    public static void main(String[] args) {
+        SoulMaster soulMaster = new SoulMaster("Pesho", 99);
+        System.out.println(soulMaster);
+    }
+}
