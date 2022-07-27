6b4f21b1217b105d5d478e106ce7189750b06a9f
Yordan Milanov <dancho.galinov@gmail.com>
2022-07-27 18:49:56 +0300

    CarShop_01 100/100

---
+++ 07.InterfacesAndAbstractions-Lab/src/CarShop_01/Car.java
@@ -0,0 +1,14 @@
+package CarShop_01;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+
+    Integer TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+
+}
