71efc47940201648f20df0faeec21085d8a9f2d4
Yordan Milanov <dancho.galinov@gmail.com>
2022-07-28 14:15:50 +0300

    MultipleImplementation_02 100/100

---
+++ 08.InterfaceAndAbstractions-Exercise/src/MultipleImplementation_02/Citizen.java
@@ -0,0 +1,34 @@
+package MultipleImplementation_02;
+
+public class Citizen implements Person, Birthable, Identifiable {
+
+    private String name;
+    private int age;
+    private String Id;
+    private String birthDate;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        Id = id;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+}
